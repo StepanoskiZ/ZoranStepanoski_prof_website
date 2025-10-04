240f4349af57bf1547b7fce11775ecd616075ab0
StepanoskiZ <zstepanoski@gmail.com>
2025-10-04 03:48:00 +0200

    Some minor fixes in admin forms.

--- src/main/webapp/app/entities/about-me/about-me.model.ts
+++ src/main/webapp/app/entities/about-me/about-me.model.ts
@@ -3,6 +3,7 @@ import { Language } from 'app/entities/enumerations/language.model';
 
 export interface IAboutMe {
   id: number;
+  name?: string | null;
   contentHtml?: string | null;
   language?: keyof typeof Language | null;
   media?: Pick<IAboutMeMedia, 'id' | 'mediaUrl'>[] | null;

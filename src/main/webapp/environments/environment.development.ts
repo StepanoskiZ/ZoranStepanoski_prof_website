c595d277dc9ffaf96b2abc28cd69081bd0c91157
StepanoskiZ <zstepanoski@gmail.com>
2025-10-12 23:40:48 +0200

    Fixing bug.

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -1,6 +1,8 @@
+declare const __ADMIN_SECRET__: string;
+
 export const environment = {
   VERSION: 'DEV',
   DEBUG_INFO_ENABLED: true,
   isAdminEnv: true,
-  ADMIN_SECRET: ADMIN_SECRET,
+  ADMIN_SECRET: __ADMIN_SECRET__,
 };
